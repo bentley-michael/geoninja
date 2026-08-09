aba9219aa1248ca61d5ea2ae9491ab4af3067280
Michael Bentley <163693235+bentley-michael@users.noreply.github.com>
2026-08-09 16:52:36 -0500

    Move GN branding into Vite app

--- geoninja/src/main.jsx
+++ geoninja/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import BrandShell from './BrandShell.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrandShell>
+      <App />
+    </BrandShell>
   </StrictMode>,
 )
